9a09e4f022c5c1d2deb31d955cd2b3ecae384033
Anurag Hazra <hazru.anurag@gmail.com>
2019-03-28 12:40:27 +0530

    Create Nothing.asm

---
+++ Nothing.asm
@@ -0,0 +1,15 @@
+section	.text
+	global _start
+_start: 
+	mov	edx, len   
+	mov	ecx, msg
+	mov	ebx, 1   
+	mov	eax, 4	    
+	int	0x80        
+	mov	eax, 1	    
+	int	0x80        
+
+section	.data
+
+msg	db	'Nothing',0xa
+len	equ	$ - msg
